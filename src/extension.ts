a28b598e0f31776e5c26fc80549e232e2b176141
Syam Sundar K <k.syamsundar21@gmail.com>
2019-06-02 09:49:33 +0530

    Init textMate syntax for Processing

--- src/extension.ts
+++ src/extension.ts
@@ -5,11 +5,10 @@ export function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "processing-extension" is now active!');
 
 	let disposable = vscode.commands.registerCommand('extension.processing', () => {
-		// Test Pop-up
+		// Test pop-up
 		vscode.window.showInformationMessage('Processing Language Server Activated.!');
 	});
 
 	context.subscriptions.push(disposable);
 }
-
 export function deactivate() {}
