f3272ba0a5a6a3b521f2815a32de4595dc215ac2
Syam Sundar K <k.syamsundar21@gmail.com>
2019-06-21 08:55:36 +0530

    AST node module added and tested

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,5 @@
 import * as vscode from 'vscode';
+import { parse } from 'java-ast'
 
 export function activate(context: vscode.ExtensionContext) {
 
@@ -7,6 +8,11 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('extension.processing', () => {
 		// Test pop-up
 		vscode.window.showInformationMessage('Processing Language Server Activated.!');
+
+		const ast = parse(`void setup() {\n} \n void draw() {\n}`);
+		// finds the starting index of `setup` in the abstract syntax tree
+		vscode.window.showInformationMessage(ast.toStringTree().search('setup').toString());
+
 	});
 
 	context.subscriptions.push(disposable);
