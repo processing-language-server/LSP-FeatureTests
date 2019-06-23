f0c35124d7f2fee52137e9b9750eb95283af0f20
Syam Sundar K <k.syamsundar21@gmail.com>
2019-06-23 09:39:55 +0530

    AST Testing

--- src/extension.ts
+++ src/extension.ts
@@ -5,14 +5,20 @@ export function activate(context: vscode.ExtensionContext) {
 
 	console.log('Congratulations, your extension "processing-extension" is now active!');
 
+	const ast = parse(`void setup() {\n} \n void draw() {\n}`);
+
 	let disposable = vscode.commands.registerCommand('extension.processing', () => {
 		// Test pop-up
 		vscode.window.showInformationMessage('Processing Language Server Activated.!');
 
-		const ast = parse(`void setup() {\n} \n void draw() {\n}`);
 		// finds the starting index of `setup` in the abstract syntax tree
 		vscode.window.showInformationMessage(ast.toStringTree().search('setup').toString());
 
+		// returns 2nd children - here `setup`
+		vscode.window.showInformationMessage(ast.getChild(1).text);
+
+		// returns total number of children - 0 to 11 -> 12
+		vscode.window.showInformationMessage(ast.childCount.toString());
 	});
 
 	context.subscriptions.push(disposable);
